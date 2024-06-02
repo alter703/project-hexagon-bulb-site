f5a67b03c2943e8ecdb9f7e817c560791f3ee500
Anatoliy <anatoliy.brazh130@gmail.com>
2024-06-02 20:37:42 +0300

    Update templates. Api has no got any changes.

--- apps/members/urls.py
+++ apps/members/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('profile/<uuid:uuid>', views.ProfileDetailView.as_view(), name='profile'),
+    path('profile/<uuid:uuid>/', views.ProfileDetailView.as_view(), name='profile'),
     path('profile/edit/', views.ProfileEditView.as_view(), name='profile-edit'),
 
     path('profile/<uuid:uuid>/questions/', views.user_questions_view, name='user_questions'),
