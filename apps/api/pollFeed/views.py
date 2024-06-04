b3bcbcdb0dfb448ca81a8d3f242de6ae57f8c8ea
Anatoliy <anatoliy.brazh130@gmail.com>
2024-06-04 18:22:00 +0300

    add profanity-check to check an offensive language while user's creating, updating Questions.

--- apps/api/pollFeed/views.py
+++ apps/api/pollFeed/views.py
@@ -6,11 +6,7 @@ from .mixins import PollMixin
 
 class PollListAPIView(PollMixin, generics.ListAPIView):
     pass
-    # queryset = Poll.objects.all()
-    # serializer_class = PollSerializer
 
 
 class PollDetailAPIView(PollMixin, generics.RetrieveAPIView):
     lookup_field = 'id'
-    # queryset = Poll.objects.all()
-    # serializer_class = PollSerializer
