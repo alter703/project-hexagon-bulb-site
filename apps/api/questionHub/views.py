db7c6f879fecd2474a8156bb3f0fcf5b1ffa8c12
Anatoliy <anatoliy.brazh130@gmail.com>
2024-06-04 14:05:10 +0300

    update views, urls and serializers: in serializers add create and update methods; from view func question_random to QuestionAPIView(to class).

--- apps/api/questionHub/views.py
+++ apps/api/questionHub/views.py
@@ -11,33 +11,29 @@ from .mixins import QuestionMixin, CategoryMixin
 
 class QuestionDetailAPIView(QuestionMixin, generics.RetrieveAPIView):
     lookup_field = 'id' 
-    # def get_serializer_class(self):
-    #     return 
+
+
+class QuestionRandomAPIView(APIView):
+    def get(self, request):
+        try:
+            question = Question.objects.order_by('?').first()
+        except Question.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
 
 
 # @api_view(['GET', ])
-# def question_detail(request, id):
-#     # print(request.data)
+# def question_random(request):
 #     try:
-#         question = Question.objects.get(id=id)
+#         question = Question.objects.order_by('?').first()
 #     except Question.DoesNotExist:
 #         return Response(status=status.HTTP_404_NOT_FOUND)
     
 #     if request.method == 'GET':
 #         serializer = QuestionSerializer(question)
 #         return Response(serializer.data)
-
-
-@api_view(['GET', ])
-def question_random(request):
-    try:
-        question = Question.objects.order_by('?').first()
-    except Question.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data)
 
 
 class QuestionAPIView(APIView):
@@ -52,22 +48,28 @@ class QuestionAskAPIView(APIView):
 
         serializer = QuestionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        question = Question.objects.create(
-            author=User.objects.get(id=request.data['author']),
-            title=request.data['title'],
-            content=request.data['content'],
-            category=Category.objects.get(id=request.data['category']),
-        )
+        return Response({'question': serializer.data})
 
-        return Response({'question': QuestionSerializer(question).data})
+    def put(self, request, *args, **kwargs):
+        q_id = kwargs.get('id', None)
+        request.data['author'] = request.user.id
+
+        if not q_id:
+            return Response({'error': "Method \"Put\" is not allowed."})
+
+        try:
+            instance = Question.objects.get(id=q_id)
+        except Exception:
+            return Response({"error": "This Question does not exists."})
+
+        serializer = QuestionSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"question": serializer.data})
 
 
 class CategoryListAPIView(CategoryMixin, generics.ListAPIView):
     pass
-
-# class CategoryAPIView(APIView):
-#     def get(self, request):
-#         categories = Category.objects.all()
-
-#         return Response({'categories': CategorySerializer(categories, many=True).data})
