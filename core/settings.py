083be96b29ab19e536621462e9c59878ecfb503e
Anatoliy <anatoliy.brazh130@gmail.com>
2024-06-10 15:53:00 +0300

    Update lots of templates. Add simple context processor is_user_authenticated.

--- core/settings.py
+++ core/settings.py
@@ -88,6 +88,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'apps.main.context_processors.is_user_authenticated'
             ],
         },
     },
