17d7a9b31bf13fcc3afacdbce52e94d577b9d8e2
Anatoliy <anatoliy.brazh130@gmail.com>
2024-06-14 16:01:09 +0300

    update urls and templates.

--- apps/questionHub/urls.py
+++ apps/questionHub/urls.py
@@ -1,19 +1,18 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'questionHub'
 
 urlpatterns = [
     path('', views.QuestionsListView.as_view(), name='index'),
-    path('question/<uuid:id>/', views.QuestionDetailView.as_view(), name='detail'),
     path('question/ask/', views.AskQuestionView.as_view(), name='ask'),
+    path('question/<uuid:id>/', views.QuestionDetailView.as_view(), name='detail'),
     path('question/<uuid:id>/answer/', views.AnswerView.as_view(), name='answer-question'),
     path('question/<uuid:id>/delete/', views.QuestionDeleteView.as_view(), name='delete'),
     path('question/<uuid:id>/update/', views.QuestionUpdateView.as_view(), name='update'),
     path('question/<uuid:id>/close/', views.CloseQuestionView.as_view(), name='close'),
-
+    path('question/<uuid:id>/bookmark/', views.BookmarkView.as_view(), name='bookmark'),
+    
     path('search/', views.QuestionsListView.as_view(), name='search'),
     path('category/<int:id>/', views.QuestionsByCategoryListView.as_view(), name='select-category'),
-    path('question/<uuid:id>/bookmark/', views.BookmarkView.as_view(), name='bookmark'),
 ]
